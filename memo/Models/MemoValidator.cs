1a23174a77f1c9c462e20f330929aa45218c4a9d
zorangligorevic <zorang@live.com.au>
2018-10-19 22:48:44 +1100

    memo validator

---
+++ memo/Models/MemoValidator.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace memo.Models
+{
+    public class MemoValidator : Memo
+    {
+        public bool isValidTitle()
+        {
+            if ((Title.Count() > 0) && (Title.Count() <= 200))
+                return true;
+            else
+                return false;
+        }
+
+        public bool isValidDateTime()
+        {
+            if (typeof(DateTime) == Date.GetType())
+                return true;
+            else
+                return false;
+            
+        }
+    }
+}
